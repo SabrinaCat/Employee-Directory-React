74cfcde8d9c6e630653f28f0ac4a72e1513c5b18
SabrinaCat <Sabrina.glow22@gmail.com>
2020-09-23 21:49:13 -0500

    added rest of employee data

--- employee_directory/src/components/table.js
+++ employee_directory/src/components/table.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import employee from './employees.json';
+//import employee from './employees.json';
+import style from './table.css'
 
 class Table extends Component {
     constructor (props) {
@@ -17,8 +18,57 @@ class Table extends Component {
                 "name": "Barbara Jensen",
                 "role": "Project Manager",
                 "email": "StopCallingMeBarb@gmail.com"  
-            }]
-        };
+            },
+            {
+            "id": 3,
+            "name": "Randi Aspinwall",
+            "role": "Intern",
+            "email": "IJustWalkTheDogs@yahoo.com"
+        },
+        {
+            "id": 4,
+            "name": "Molly Gilbert",
+            "role": "Senior Developer",
+            "email": "Iamnot42@gmail.com"
+        },
+        {
+            "id": 5,
+            "name": "Billy Hollandaise",
+            "role": "Senior Developer",
+            "email": "MynewfavoritePun@yahoo.com"
+        },
+        {
+            "id": 6,
+            "name": "Billy Eyelash",
+            "role": "Intern",
+            "email": "CallmeBillyStylish@gmail.com"
+        },
+        {
+            "id": 7,
+            "name": "Ella Bella",
+            "role": "Intern",
+            "email": "Bestdogintheoffice@gmail.com"
+        },
+        {
+            "id": 8,
+            "name": "Mittens Myers",
+            "role": "Junior Developer",
+            "email": "Thesepeopleareanimals@myersllc.com"
+        },
+        {
+            "id": 9,
+            "name": "Jeff Cartangena",
+            "role": "Project Manager",
+            "email": "SourceofTruth@gmail.com"
+        },
+        {
+            "id": 10,
+            "name": "Leslie Nielsen",
+            "role": "Junior Developer",
+            "email": "DontcallmeShirley@Airplane.com"
+        }
+    ]
+};
             //employees: {employee}
         
     }
@@ -37,7 +87,7 @@ renderTableData(){
 }
 render() {
     return (
-        <div>
+        <div style={style}>
             <h1>Current Employees</h1>
             <table id="employees">
                 <tbody>
